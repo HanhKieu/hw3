ecbf75ff0e8b170d22ca4e085897884ecab15415
Hanh Kieu <hckieu@ucdavis.edu>
2016-02-08 18:27:11 -0800

    implemented Sequnce as a node

--- hw3/part1/Sequence.java
+++ hw3/part1/Sequence.java
@@ -1,14 +1,27 @@
 public class Sequence extends Element {
-	Element[] mySeqVar;
-    public Sequence(){
-    	mySeqVar = new Element[0];
-    }
+	Sequence next;
+	Element data;
 
-    public Element[] Get(){
-    	return mySeqVar;
-    }
 
-    public void Set(Element[] val){
-    	mySeqVar = val;
-    }
+	public Sequence(){
+    	next = null;
+    }//no argument in constructor
+
+    public Sequence(Element val){
+    	next = null;
+    	data = val;
+    }//create a sequence with a data value
+
+    public Sequence(Element val, Sequence nextValue){
+    	next = nextValue;
+    	data = val;
+    }//create a sequence with a data value, and define what it points to next in the Sequence
+
+    // public Sequence Get(){
+    // 	return mySeqVar;
+    // }
+
+    // public void Set(Sequence val){
+    // 	mySeqVar = val;
+    // }
 }
